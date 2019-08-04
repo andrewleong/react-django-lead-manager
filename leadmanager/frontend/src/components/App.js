da6978713d4efbeeb96c3daf4fd04eec5168a7cd
andrewleong <andrewleong1993@gmail.com>
2019-08-04 22:13:41 +0800

    Add Lead components

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -1,10 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import ReactDOM from 'react-dom'
+
+import Header from './Layout/Header'
+import Dashboard from './Leads/Dashboard'
 
 class App extends Component {
     render() {
         return (
-            <hi>Hello</hi>
+            <Fragment>
+                <Header />
+                <div className="container">
+                    <Dashboard />
+                </div>
+            </Fragment>
         )
     }
 }
