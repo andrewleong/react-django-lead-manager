64e154284a098410175cb58b8ac789b53d064851
andrewleong <andrewleong1993@gmail.com>
2019-11-02 21:08:01 +0800

    Manually set up react as frontend app

---
+++ leadmanager/frontend/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+function App() {
+    return (
+        <div>
+            <h1>App</h1>
+        </div>
+    )
+}
+
+ReactDom.render(<App/>, document.querySelector('#app'))
+
